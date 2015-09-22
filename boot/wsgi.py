188dd9a80ef29c2d906dc4fe6d47cb0f45bcdbd0
J. David Ibáñez <jdavid.ibp@gmail.com>
2015-09-22 19:23:02 +0200

    wsgi: rewrite ping middleware

--- boot/wsgi.py
+++ boot/wsgi.py
@@ -10,21 +10,19 @@ features:
 from project.wsgi import application
 
 
-class PingMiddleware:
-
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
+def ping_middleware(app):
+    def ping(environ, start_response, app=app):
         if environ['PATH_INFO'] == '/ping':
-            # we're being asked to render the profile view
             text = 'pong'.encode('utf-8')
             start_response('200 OK', [
                 ('Content-type', 'text/plain; charset="utf-8"'),
                 ('Content-length', str(len(text)))])
             return [text]
 
-        return self.app(environ, start_response)
+        return app(environ, start_response)
+
+    return ping
+
 
 # Add url /ping for monitoring
-application = PingMiddleware(application)
+application = ping_middleware(application)
