cfe3e4cc540333e0839f25a0fed4f057c1751054
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-06-11 18:42:49 +0200

    wsn_fields_invalid fine grained control

--- wsn/management/commands/wsn_fields_invalid.py
+++ wsn/management/commands/wsn_fields_invalid.py
@@ -3,24 +3,30 @@ from django.core.management.base import BaseCommand
 from wsn.models import Frame
 
 
+def get_suffix(delete):
+    if delete:
+        return 'frames deleted'
+
+    return 'frames would be deleted'
+
+
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('--delete', action='store_true', default=False)
+        parser.add_argument('--delete-old', action='store_true', default=False)
+        parser.add_argument('--delete-bad', action='store_true', default=False)
 
     def handle(self, *args, **kw):
-        delete = kw['delete']
-        if delete:
-            suffix = 'frames deleted'
-        else:
-            suffix = 'frames would be deleted'
+        delete_old = kw['delete-old']
+        delete_bad = kw['delete-bad']
 
         # Old date
         date = datetime.date(2001, 1, 1)
         frames = Frame.objects.filter(time__lt=date)
-        if delete:
+        if delete_old:
             n, _ = frames.delete()
         else:
             n = frames.count()
+        suffix = get_suffix(delete_old)
         print('Invalid time value: {} {}'.format(n, suffix))
 
         # DS1820
@@ -29,7 +35,8 @@ class Command(BaseCommand):
             value = frame.data['ds1820']
             if type(value) is not list:
                 n += 1
-                if delete:
+                if delete_bad:
                     frame.delete()
 
+        suffix = get_suffix(delete_bad)
         print('Invalid DS1820 value: {} {}'.format(n, suffix))
