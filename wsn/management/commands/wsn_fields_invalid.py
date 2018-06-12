456f60e5fe346b6a9db51c664eab3702a3469653
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-06-12 09:31:07 +0200

    wsn_fields_invalid fix typo

--- wsn/management/commands/wsn_fields_invalid.py
+++ wsn/management/commands/wsn_fields_invalid.py
@@ -3,21 +3,19 @@ from django.core.management.base import BaseCommand
 from wsn.models import Frame
 
 
-def get_suffix(delete):
-    if delete:
-        return 'frames deleted'
-
-    return 'frames would be deleted'
-
-
 class Command(BaseCommand):
+
     def add_arguments(self, parser):
         parser.add_argument('--delete-old', action='store_true', default=False)
         parser.add_argument('--delete-bad', action='store_true', default=False)
 
+    def log(self, prefix, n, delete):
+        suffix = 'frames deleted' if delete else 'frames would be deleted'
+        print('{}: {} {}'.format(prefix, n, suffix))
+
     def handle(self, *args, **kw):
-        delete_old = kw['delete-old']
-        delete_bad = kw['delete-bad']
+        delete_old = kw['delete_old']
+        delete_bad = kw['delete_bad']
 
         # Old date
         date = datetime.date(2001, 1, 1)
@@ -26,10 +24,9 @@ class Command(BaseCommand):
             n, _ = frames.delete()
         else:
             n = frames.count()
-        suffix = get_suffix(delete_old)
-        print('Invalid time value: {} {}'.format(n, suffix))
+        self.log('Invalid time value', n, delete_old)
 
-        # DS1820
+        # DS1820 bad values (not list)
         n = 0
         for frame in Frame.objects.filter(data__has_key='ds1820'):
             value = frame.data['ds1820']
@@ -37,6 +34,4 @@ class Command(BaseCommand):
                 n += 1
                 if delete_bad:
                     frame.delete()
-
-        suffix = get_suffix(delete_bad)
-        print('Invalid DS1820 value: {} {}'.format(n, suffix))
+        self.log('Invalid DS1820 value', n, delete_bad)
