f0cdc1a68a9d06edbe81dc6e2d80107053a54b9b
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-06-12 20:08:17 +0200

    wsn_fields_invalid ds1820 set bad values to none

--- wsn/management/commands/wsn_fields_invalid.py
+++ wsn/management/commands/wsn_fields_invalid.py
@@ -6,32 +6,46 @@ from wsn.models import Frame
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
-        parser.add_argument('--delete-old', action='store_true', default=False)
-        parser.add_argument('--delete-bad', action='store_true', default=False)
+        parser.add_argument('-p', '--pretend', action='store_true', default=False)
 
-    def log(self, prefix, n, delete):
-        suffix = 'frames deleted' if delete else 'frames would be deleted'
-        print('{}: {} {}'.format(prefix, n, suffix))
+    def __log(self, prefix, n, suffix):
+        mod = ['would be'] if self.pretend else []
+        msg = ' '.join([prefix, str(n), 'frames'] + mod + [suffix])
+        self.stdout.write(msg)
 
-    def handle(self, *args, **kw):
-        delete_old = kw['delete_old']
-        delete_bad = kw['delete_bad']
-
-        # Old date
+    def __delete_old(self):
         date = datetime.date(2001, 1, 1)
         frames = Frame.objects.filter(time__lt=date)
-        if delete_old:
-            n, _ = frames.delete()
-        else:
+        if self.pretend:
             n = frames.count()
-        self.log('Invalid time value', n, delete_old)
+        else:
+            n, _ = frames.delete()
+        self.__log('Invalid time value:', n, 'deleted')
 
-        # DS1820 bad values (not list)
+    def __ds1820_value(self, value):
+        if (value is not None) and not (-100 < value < 100):
+            return None
+
+        return value
+
+    def __fix_ds1820(self):
+        key = 'ds1820'
         n = 0
-        for frame in Frame.objects.filter(data__has_key='ds1820'):
-            value = frame.data['ds1820']
-            if type(value) is not list:
+        for frame in Frame.objects.filter(data__has_key=key):
+            value = frame.data[key]
+            if type(value) is list:
+                new_value = [self.__ds1820_value(x) for x in value]
+            else:
+                new_value = self.__ds1820_value(value)
+
+            if new_value != value:
+                if not self.pretend:
+                    frame.data[key] = new_value
+                    frame.save()
                 n += 1
-                if delete_bad:
-                    frame.delete()
-        self.log('Invalid DS1820 value', n, delete_bad)
+        self.__log('Invalid DS1820 value:', n, 'fixed')
+
+    def handle(self, *args, **kw):
+        self.pretend = kw['pretend']
+        self.__delete_old()
+        self.__fix_ds1820()
