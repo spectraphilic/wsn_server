9cfe454efec6f5f5bf7a8c51d8b678ec7a5a65f2
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-09-05 12:41:00 +0200

    Improvements to the motes import command

--- wsn/management/commands/motes.py
+++ wsn/management/commands/motes.py
@@ -1,5 +1,7 @@
 # Standard Library
+from datetime import datetime
 import re
+from time import time
 import zipfile
 
 import tqdm
@@ -10,6 +12,7 @@ from django.core.management.base import BaseCommand
 # Project
 from wsn.api import frame_to_database
 from wsn.parsers import waspmote
+from wsn.settings import WSN_MIN_DATE
 
 
 class Command(BaseCommand):
@@ -19,32 +22,78 @@ class Command(BaseCommand):
                             help='Path to file or directory')
 
     def handle(self, *args, **kw):
-        expr = re.compile('.*DATA/[0-9]{6}\.TXT$')
+        expr = re.compile(r'.*DATA/([0-9]{6})\.TXT$')
+
+        series = {}
+        state = {}
 
         # Parse
-        frames = []
+        skipped = []
         for path in kw['paths']:
             assert zipfile.is_zipfile(path)
             self.stdout.write('Parsing %s' % path)
             with zipfile.ZipFile(path, 'r') as zf:
-                names = zf.namelist()
+                names = sorted(zf.namelist())
                 for name in tqdm.tqdm(names):
-                    if expr.match(name):
+                    match = expr.match(name)
+                    if match:
+                        date = '20' + match.group(1)
+                        date = datetime.strptime(date, '%Y%m%d').date()
+                        if date < WSN_MIN_DATE.date():
+                            skipped.append(name)
+                            continue
+
                         with zf.open(name) as data_file:
                             for frame in waspmote.read_wasp_data(data_file):
+                                # Add name to frame if it's missing
+                                serial = frame['serial']
+                                name = frame['name']
+                                if serial not in state or frame['name']:
+                                    state[serial] = name
+                                else:
+                                    name = frame['name'] = state[serial]
+
+                                # Preprocess frame, changes from flat structure
+                                # to {'tags': {}, 'frames': []} structure
                                 frame = waspmote.data_to_json(frame)
-                                frames.append(frame)
 
-        # Sort by time
-        frames.sort(key=lambda x: x['frames'][0]['time'])
-        first = frames[0]['frames'][0]['time']
-        last = frames[-1]['frames'][0]['time']
-        assert first < last
+                                # Discard bad frames (TODO)
+#                               time = frame['frames'][0]['time']
+#                               time = datetime.strptime(time, '%Y-%m-%dT%H:%M:%S+00:00')
+#                               if time.date() != date:
+#                                   print(name)
+#                                   print(frame)
+#                                   continue
 
-        # Inserting
-        self.stdout.write('Inserting %d frames into the database' % len(frames))
-        self.stdout.write('From %s to %s' % (first, last))
+                                # Append frame to series
+                                key = (serial, name)
+                                if key not in series:
+                                    series[key] = frame
+                                else:
+                                    series[key]['frames'].extend(frame['frames'])
 
-        # Insert
-        for frame in tqdm.tqdm(frames):
-            frame_to_database(frame, update=False)
+
+        # Skipped data files
+        if skipped:
+            self.stdout.write('Data files skipped: %s' % skipped)
+
+        # Import series
+        for serie in series.values():
+            tags = serie['tags']
+            frames = serie['frames']
+
+            # Sort by time
+            #frames.sort(key=lambda x: x['time'])
+            first = frames[0]['time']
+            last = frames[-1]['time']
+            assert first < last
+
+            # Inserting
+            self.stdout.write('')
+            self.stdout.write('serial={serial} name={name}'.format(**tags))
+            self.stdout.write('{} frames from {} to {}'.format(len(frames), first, last))
+            yes = input('Insert into database? Yes/[No]: ')
+            if yes.lower() == 'yes':
+                t0 = time()
+                frame_to_database(serie, update=False)
+                self.stdout.write('Done in 5{} seconds'.format(time() - t0))
---
+++ wsn/settings.py
@@ -0,0 +1,3 @@
+from datetime import datetime, timezone
+
+WSN_MIN_DATE = datetime(2017, 11, 1, tzinfo=timezone.utc)
