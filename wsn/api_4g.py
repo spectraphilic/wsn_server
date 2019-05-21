8a43a3ef95f8cc07fd675e364408402e3c7259c9
J. David Ibáñez <jdavid.ibp@gmail.com>
2019-05-21 18:17:04 +0200

    4G: use Celery (not tested)

--- wsn/api_4g.py
+++ wsn/api_4g.py
@@ -1,9 +1,5 @@
 # Standard Library
-import base64
-import datetime
 import time
-
-import pytz
 
 # Django
 from django.http import HttpResponse
@@ -12,57 +8,7 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 # App
-from .models import Frame, frame_to_database
-from .parsers import waspmote
-
-
-oslo = pytz.timezone('Europe/Oslo')
-def epoch_to_oslo(epoch):
-    """
-    Convert Unixe epoch time to local Europe/Oslo time.
-    """
-    dt = datetime.datetime.utcfromtimestamp(epoch)
-    dt = datetime.datetime.utcfromtimestamp(epoch).replace(tzinfo=pytz.utc)
-    return dt.astimezone(oslo)
-
-
-def postfix(frame, save=False, verbose=False):
-    """
-    Returns None if there's nothing to fix.
-    Returns True if the frame has been fixed, False if it has not.
-    """
-    name = 'sw-002'
-    if frame.metadata.name != name:
-        return None
-
-    # The bad data goes from 00:10 to 02:00 local time
-    # Or from 00:05 to 01:00 (from March 14 to March 20)
-    dt = epoch_to_oslo(frame.time)
-    if datetime.time(0) < dt.time() <= datetime.time(2):
-        # Find out the time distance from the previous frame
-        frames = Frame.objects.filter(metadata__name=name).order_by('id')
-        prev = frames.filter(pk__lt=frame.pk).last()
-        diff = frame.time - prev.time
-
-        # The clock jumps +1 day, so the time difference between the 2
-        # consecutive frames is greather than 1 day. We add a margin
-        # error of 2h for the upper limit, so we don't catch too much.
-        one_hour = 3600
-        one_day = 24 * 3600
-        if one_day < diff < one_day + one_hour * 2:
-            old_time = frame.time
-            new_time = old_time - one_day
-            if verbose:
-                old_dt = datetime.datetime.utcfromtimestamp(old_time)
-                new_dt = datetime.datetime.utcfromtimestamp(new_time)
-                print(f'Fix {frame.pk} {old_dt} -> {new_dt}')
-
-            if save:
-                frame.time = new_time
-                frame.save()
-                return True
-
-    return False
+from . import tasks
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -72,30 +18,20 @@ class MeshliumView(View):
     """
 
     def post(self, request, *args, **kwargs):
-        remote_addr = request.META.get('REMOTE_ADDR', '')
         datas = request.POST.get('frame')
         if type(datas) is str:
             datas = [datas]
 
-        for data in datas:
-            # Parse frame
-            data = base64.b16decode(data)
-            while data:
-                frame, data = waspmote.parse_frame(data)
-                if frame is None:
-                    break
+        # Received
+        kw = {}
+        kw['received'] = int(time.time())
 
-                # Add received time and remote address
-                frame['received'] = int(time.time())
-                if remote_addr:
-                    frame['remote_addr'] = remote_addr
+        # Remote address
+        remote_addr = request.META.get('REMOTE_ADDR', '')
+        if remote_addr:
+            kw['remote_addr'] = remote_addr
 
-                # Save to database
-                validated_data = waspmote.data_to_json(frame)
-                metadata, objs = frame_to_database(validated_data)
-
-                # Fix time in sw-002
-                for obj in objs:
-                    postfix(obj, save=True, verbose=False)
+        # Send task to worker
+        tasks.in_4G.delay(datas, **kw)
 
         return HttpResponse(status=200)
--- wsn/tasks.py
+++ wsn/tasks.py
@@ -1,19 +1,104 @@
 # Standard Library
+import base64
 import codecs
-from datetime import datetime
+import datetime
+
+import pytz
 
 # Celery
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
 # App
-from .models import frame_to_database
+from .models import Frame, frame_to_database
 from .parsers import waspmote
 
 
 logger = get_task_logger(__name__)
 
 
+#
+# 4G
+#
+oslo = pytz.timezone('Europe/Oslo')
+def epoch_to_oslo(epoch):
+    """
+    Convert Unixe epoch time to local Europe/Oslo time.
+    """
+    dt = datetime.datetime.utcfromtimestamp(epoch)
+    dt = datetime.datetime.utcfromtimestamp(epoch).replace(tzinfo=pytz.utc)
+    return dt.astimezone(oslo)
+
+
+def postfix(frame, save=False, verbose=False):
+    """
+    Returns None if there's nothing to fix.
+    Returns True if the frame has been fixed, False if it has not.
+    """
+    name = 'sw-002'
+    if frame.metadata.name != name:
+        return None
+
+    # The bad data goes from 00:10 to 02:00 local time
+    # Or from 00:05 to 01:00 (from March 14 to March 20)
+    dt = epoch_to_oslo(frame.time)
+    if datetime.time(0) < dt.time() <= datetime.time(2):
+        # Find out the time distance from the previous frame
+        frames = Frame.objects.filter(metadata__name=name).order_by('id')
+        prev = frames.filter(pk__lt=frame.pk).last()
+        diff = frame.time - prev.time
+
+        # The clock jumps +1 day, so the time difference between the 2
+        # consecutive frames is greather than 1 day. We add a margin
+        # error of 2h for the upper limit, so we don't catch too much.
+        one_hour = 3600
+        one_day = 24 * 3600
+        if one_day < diff < one_day + one_hour * 2:
+            old_time = frame.time
+            new_time = old_time - one_day
+            if verbose:
+                old_dt = datetime.datetime.utcfromtimestamp(old_time)
+                new_dt = datetime.datetime.utcfromtimestamp(new_time)
+                print(f'Fix {frame.pk} {old_dt} -> {new_dt}')
+
+            if save:
+                frame.time = new_time
+                frame.save()
+                return True
+
+    return False
+
+
+@shared_task(
+    acks_late=True,             # Send ack at the end, not the beginning
+    autoretry_for=(Exception,), # Retry for any exception
+    max_retries=None,           # Retry for ever
+    default_retry_delay=300,    # Retry after 5min (default is 3 minutes)
+)
+def in_4G(datas, **kw):
+    for data in datas:
+        # Parse frame
+        data = base64.b16decode(data)
+        while data:
+            frame, data = waspmote.parse_frame(data)
+            if frame is None:
+                break
+
+            # Add received time and remote address
+            frame.update(kw)
+
+            # Save to database
+            validated_data = waspmote.data_to_json(frame)
+            metadata, objs = frame_to_database(validated_data)
+
+            # Fix time in sw-002
+            for obj in objs:
+                postfix(obj, save=True, verbose=False)
+
+
+#
+# Iridium
+#
 @shared_task(
     acks_late=True,             # Send ack at the end, not the beginning
     autoretry_for=(Exception,), # Retry for any exception
@@ -53,7 +138,7 @@ def in_iridium(POST):
 
     # Convert
     data = codecs.decode(data, 'hex')
-    transmit_time = datetime.strptime(transmit_time, '%y-%m-%d %H:%M:%S')
+    transmit_time = datetime.datetime.strptime(transmit_time, '%y-%m-%d %H:%M:%S')
     transmit_time = int(transmit_time.timestamp())
 
     # Catch test messages
