d7b5574f70e449c0fd8b08d2d0bc0f5f8f3c7daf
J. David Ibáñez <jdavid.ibp@gmail.com>
2019-05-22 19:06:33 +0200

    Use pytest

--- wsn/tests.py
+++ wsn/tests.py
@@ -3,97 +3,112 @@ from datetime import datetime
 import time
 
 #import dateutil
+import pytest
 
 # Django
 from django.contrib.auth import get_user_model
 
 # Rest Framework
 from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
 # WSN
 from wsn.models import Frame
 
 
-class CreateTestCase(APITestCase):
-    def setUp(self):
-        User = get_user_model()
-        user = User.objects.create(username='api')
-        self.token = Token.objects.create(user=user).key
-        self.extra = {'HTTP_AUTHORIZATION': 'Token %s' % self.token}
-
-    def post(self, url, data, extra):
-        return self.client.post(url, data, format='json', **extra)
-
-    def test_create_time_required(self):
-        response = self.post('/api/create/',
-            {
-                'tags': {'serial': 42},
-                'frames':
-                    [
-                        {'data': {'battery': 50}},
-                        {'data': {'battery': 75}},
-                        {'data': {'battery': 30}},
-                    ]
-            }, self.extra)
-        self.assertEqual(response.status_code, 400)
-
-    def test_create_time_badtype(self):
-        ts = datetime.utcnow()
-        response = self.post('/api/create/',
-            {
-                'tags': {'serial': 42},
-                'frames':
-                    [
-                        {'time': ts.isoformat(), 'data': {'battery': 99}},
-                    ]
-            }, self.extra)
-        self.assertEqual(response.status_code, 400)
-
-    def test_create(self):
-        # Create
-        now = int(time.time())
-        t = int(time.time())
-        response = self.post('/api/create/',
-            {
-                'tags': {'serial': 42},
-                'frames':
-                    [
-                        {'time': now + 0, 'data': {'battery': 50, 'received': t+0}},
-                        {'time': now + 1, 'data': {'battery': 75, 'received': t+1}},
-                        {'time': now + 2, 'data': {'battery': 30, 'received': t+2}},
-                    ]
-            }, self.extra)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(Frame.objects.count(), 3)
-        last = Frame.objects.order_by('received').last()
-        self.assertEqual(last.received, t+2)
-        self.assertEqual(last.data['battery'], 30)
-
-        # Query (miss)
-        response = self.client.get('/api/query/v2/', {'serial:int': 1234}, **self.extra)
-        self.assertEqual(response.status_code, 200)
-        json = response.json()
-        self.assertEqual(len(json['results']), 0)
-
-        # Query (hit)
-        response = self.client.get('/api/query/v2/', {'serial:int': 42}, **self.extra)
-        self.assertEqual(response.status_code, 200)
-        results = response.json()['results']
-        self.assertEqual(len(results), 3)
-        last = results[-1]
-        self.assertEqual(last['battery'], 30)
-        self.assertEqual(last['received'], t+2)
-        #self.assertEqual(now, dateutil.parser.parse(last['time']))
-
-        # Time
-        query = {'serial:int': 42, 'time__gte': now + 1}
-        response = self.client.get('/api/query/v2/', query, **self.extra)
-        self.assertEqual(response.status_code, 200)
-        json = response.json()
-        self.assertEqual(len(json['results']), 2)
+# TODO Change scope to module, create the user once
+@pytest.fixture
+def token(db):
+    User = get_user_model()
+    user = User.objects.create(username='api')
+    return Token.objects.create(user=user).key
 
 
-#   def test_query_bad_request(self):
-#       response = self.client.get('/api/query/v2/', {}, **self.extra)
-#       self.assertEqual(response.status_code, 400)
+@pytest.fixture
+def client(token):
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+    return client
+
+
+@pytest.fixture
+def api_create(client):
+    path = '/api/create/'
+    def post_json(data):
+        response = client.post(path, data, format='json')
+        return response
+
+    return post_json
+
+
+def test_create_time_required(api_create):
+    response = api_create({
+        'tags': {'serial': 42},
+        'frames':
+            [
+                {'data': {'battery': 50}},
+                {'data': {'battery': 75}},
+                {'data': {'battery': 30}},
+            ]
+    })
+    assert response.status_code == 400
+
+
+def test_create_time_badtype(api_create):
+    ts = datetime.utcnow()
+    response = api_create({
+        'tags': {'serial': 42},
+        'frames':
+            [
+                {'time': ts.isoformat(), 'data': {'battery': 99}},
+            ]
+    })
+    assert response.status_code == 400
+
+
+def test_create(client, api_create):
+    # Create
+    now = int(time.time())
+    t = int(time.time())
+    response = api_create({
+        'tags': {'serial': 42},
+        'frames':
+            [
+                {'time': now + 0, 'data': {'battery': 50, 'received': t+0}},
+                {'time': now + 1, 'data': {'battery': 75, 'received': t+1}},
+                {'time': now + 2, 'data': {'battery': 30, 'received': t+2}},
+            ]
+    })
+    assert response.status_code == 201
+    assert Frame.objects.count() == 3
+    last = Frame.objects.order_by('received').last()
+    assert last.received == t+2
+    assert last.data['battery'] == 30
+
+    # Query (miss)
+    response = client.get('/api/query/v2/', {'serial:int': 1234})
+    assert response.status_code == 200
+    json = response.json()
+    assert len(json['results']) == 0
+
+    # Query (hit)
+    response = client.get('/api/query/v2/', {'serial:int': 42})
+    assert response.status_code == 200
+    results = response.json()['results']
+    assert len(results) == 3
+    last = results[-1]
+    assert last['battery'] == 30
+    assert last['received'] == t+2
+    #assert now == dateutil.parser.parse(last['time'])
+
+    # Time
+    query = {'serial:int': 42, 'time__gte': now + 1}
+    response = client.get('/api/query/v2/', query)
+    assert response.status_code == 200
+    json = response.json()
+    assert len(json['results']) == 2
+
+
+#def test_query_bad_request(client):
+#    response = client.get('/api/query/v2/', {})
+#    assert response.status_code == 400
