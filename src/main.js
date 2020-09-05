e6f09bd954f5d765d9862843f5f9d3fb62a7cef0
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-09-05 12:58:10 +0200

    Rollup, export an object instead of a function
    This way we can export tons of stuff.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import Hello from './Hello.svelte';
 
-const router = {Hello};
 
-export default function(appId, props = {}, target = null) {
-  const appClass = router[appId];
-  const app = new appClass({
-    target: document.querySelector(target || '#anchor'),
-    props: props
-  });
-}
+export default {
+  Hello,
+};
