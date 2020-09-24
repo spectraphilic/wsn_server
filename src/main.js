918691e563b26c6d566499e642097ee552ed824b
J. David Ibáñez <jdavid.ibp@gmail.com>
2020-09-24 13:58:08 +0200

    Svelte: run JS code on document load

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import Hello from './Hello.svelte';
 export default {
   Hello,
 };
+
+
+// This will be executed once the document has been loaded
+window.addEventListener('load', function() {
+  //console.debug('Document ready');
+});
