99bd2f52b3b7084e379b599daa9634e3737c73ef
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-12-08 19:03:50 +0100

    Merge ansible/wsgi.py into project/wsgi.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+# Add url /ping for monitoring
+def ping_middleware(app):
+    def ping(environ, start_response, app=app):
+        if environ['PATH_INFO'] == '/ping':
+            text = 'pong'.encode('utf-8')
+            start_response('200 OK', [
+                ('Content-type', 'text/plain; charset="utf-8"'),
+                ('Content-length', str(len(text)))])
+            return [text]
+
+        return app(environ, start_response)
+
+    return ping
+
+application = ping_middleware(application)
