739be1e70f0958751dd291ff2c7707e20d731fea
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-12-13 12:55:35 +0100

    Clean main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,3 @@
 import Hello from './Hello.svelte';
 
 export { Hello };
-
-//console.log('main.js exports', app);
-
-// This will be executed once the document has been loaded
-window.addEventListener('load', function() {
-  //console.debug('Document ready');
-});
