d0ec6b5a61dd577adf143502836d9ce4281d0f78
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-03-08 13:44:25 +0100

    Clean code

--- apps/api/schema.py
+++ apps/api/schema.py
@@ -20,25 +20,3 @@ class Mutation:
 
 
 schema = sb.Schema(query=Query, mutation=Mutation)
-
-
-"""
-query A {
-	user(pk: 1) {
-    id
-    firstName
-  }
-}
-
-mutation M($id: ID!, $firstName: String) {
-  updateUser(data: {id: $id, firstName: $firstName}) {
-    id
-    firstName
-  }
-}
-
-{
-  "id": 1,
-  "firstName": "The Boss"
-}
-"""
