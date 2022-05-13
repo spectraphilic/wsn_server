79009cc79e7210762c8b7f7db786964e5b4f68ba
J. David Ibáñez <jdavid.ibp@gmail.com>
2022-05-13 17:56:45 +0200

    Vite: different conf for production and development

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,34 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/static/',
-  build: {
-    lib: {
-        entry: 'src/main.js',
-        formats: ['es'],
-        fileName: (format) => 'main.js',
-    },
-    manifest: true,
-    outDir: 'var/build',
-  },
-  plugins: [
-    svelte(),
-  ]
+export default defineConfig(({ command, mode }) => {
+    let entry = 'src/main.js';
+    let config = {
+        base: '/static/',
+        build: {
+            manifest: true,
+            outDir: 'var/build',
+            rollupOptions: {
+            }
+        },
+        plugins: [
+            svelte({}),
+        ]
+    }
+
+    if (command === 'serve') {
+        config.build.rollupOptions = {
+            input: {
+                main: entry,
+            }
+        }
+    } else { // build
+        config.build.lib = {
+            entry,
+            formats: ['es'],
+            fileName: (format) => 'main.js',
+        }
+    }
+
+    return config;
 })
