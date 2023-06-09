d38f65ac62eaf404b10488a558a31a3aa9f7f1a7
J. David Ibáñez <jdavid.ibp@gmail.com>
2023-06-09 12:36:30 +0200

    media in development

--- project/urls.py
+++ project/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import include, path
@@ -28,3 +30,6 @@ urlpatterns = [
     path('demo/', include('demo.urls')),
     path('ping', ping), # Do not remove
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
