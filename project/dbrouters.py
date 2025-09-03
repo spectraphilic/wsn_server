66263d9a2c005e9c9022bdbc18ba4c1179369077
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-09-03 17:07:30 +0200

    Fix ClickHouseRouter.allow_migrate

--- project/dbrouters.py
+++ project/dbrouters.py
@@ -18,20 +18,23 @@ class ClickHouseRouter:
         for model in get_subclasses(ClickhouseModel):
             if model._meta.abstract:
                 continue
-            self.route_model_names.add(model._meta.label_lower)
+            self.route_model_names.add(model._meta.label)
 
     def db_for_read(self, model, **hints):
-        if model._meta.label_lower in self.route_model_names or hints.get("clickhouse"):
+        name = model._meta.label
+        if name in self.route_model_names or hints.get("clickhouse"):
             return "clickhouse"
         return None
 
     def db_for_write(self, model, **hints):
-        if model._meta.label_lower in self.route_model_names or hints.get("clickhouse"):
+        name = model._meta.label
+        if name in self.route_model_names or hints.get("clickhouse"):
             return "clickhouse"
         return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if f"{app_label}.{model_name}" in self.route_model_names or hints.get("clickhouse"):
+        name = f"{app_label}.{model_name}"
+        if name in self.route_model_names or hints.get("clickhouse"):
             return db == "clickhouse"
         elif db == "clickhouse":
             return False
