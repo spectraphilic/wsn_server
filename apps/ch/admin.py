66e92f6fb49eecfcb4494ea3caebee3bee7b5919
J. David Ibáñez <jdavid.ibp@gmail.com>
2025-10-31 12:52:33 +0100

    admin clickhouse show all

--- apps/ch/admin.py
+++ apps/ch/admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib import admin
 from rangefilter.filters import DateTimeRangeFilter
 
@@ -11,6 +13,7 @@ def get_fields(model, replace=None):
 
     return [replace.get(field.name, field.name) for field in model._meta.get_fields()]
 
+
 class ReadOnlyModelAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
@@ -21,6 +24,45 @@ class ReadOnlyModelAdmin(admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+
+def make_readonly_admin_with_formatted_timestamp(model_class, timestamp_field='timestamp'):
+    class DynamicTimestampAdmin(ReadOnlyModelAdmin):
+        def __init__(self, model, admin_site):
+            replace = {timestamp_field: 'formatted_timestamp'}
+            self.list_display = get_fields(model, replace=replace)
+            super().__init__(model, admin_site)
+
+        @admin.display(description=timestamp_field.capitalize(), ordering=timestamp_field)
+        def formatted_timestamp(self, obj):
+            ts_value = getattr(obj, timestamp_field)
+            if ts_value is None:
+                return None
+            # Optional: handle milliseconds (remove if all timestamps are in seconds)
+            if ts_value > 1e10:
+                ts_value /= 1000
+            dt = datetime.datetime.fromtimestamp(ts_value, tz=datetime.timezone.utc)
+            return dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    DynamicTimestampAdmin.__name__ = f'{model_class.__name__}Admin'
+    return DynamicTimestampAdmin
+
+
+models_to_register = [
+    models_unmanaged.FinseSommer,
+    models_unmanaged.FinsefluxBiomet,
+    models_unmanaged.FinsefluxStationstatus,
+    models_unmanaged.GruvebadetData,
+    models_unmanaged.GruvebadetDiagnostic,
+    models_unmanaged.Mammamia3Mm3Borehole,
+    models_unmanaged.Mammamia3Mm3Surface,
+    models_unmanaged.Mobileflux2Biomet,
+    models_unmanaged.MobilefluxBiomet,
+    models_unmanaged.MobilefluxStationstatus,
+]
+
+for model in models_to_register:
+    admin.site.register(model, make_readonly_admin_with_formatted_timestamp(model))
 
 
 @admin.register(models_unmanaged.FinsefluxHfdata)
