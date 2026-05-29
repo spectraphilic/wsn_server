52f8a6c7e4a55e39406d93cd8a52f56cffdbfcfe
J. David Ibáñez <jdavid.ibp@gmail.com>
2026-05-29 16:40:23 +0200

    Fix for Django 6 and uvicorn: X-Forwarded-Port

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.views import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.views import serve
 from django.urls import path
 
 from .urls_ansible import urlpatterns
